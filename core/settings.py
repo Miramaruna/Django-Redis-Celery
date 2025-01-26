75e70e4413a55e97c5773dc33d927b7523eb80a3
Miramaruna <miruna2023kk@gmail.com>
2025-01-26 14:58:23 +0600

    serttings

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['192.168.1.104', '127.0.0.1', 'localhost']
 
 
 # Application definition
